6214aedc7ce30f08a5cf2e0d85bae871665190cc
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2020-12-12 20:52:40 +0000

    the_circumference_of_a_circle.py
    git commit -m the_circumference_of_a_circle.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created by: Mohammad-al-buraiki
+# Created on December 2020
+# This program calculates the circumference of a circle
+
+import math
+τ = 2*(math.pi)
---
+++ the_circumference_of_a_circle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Mohammad-al-buraiki
+# Created on December 2020
+# This program calculates the circumference of a circle
+
+import constants
+
+
+def main():
+    # this function calculates the circumference of a circle
+    
+    # input
+    radius = input("Enter the radius a circle (cm):")
+    
+    # process
+    circumference = constants.τ*(int(radius))
+    
+    # output
+    print("The circumference of a circle with radius {0} cm is {1} cm."
+         .format(radius, circumference))
+         
+         
+if __name__ == "__main__":
+    main()
+    
